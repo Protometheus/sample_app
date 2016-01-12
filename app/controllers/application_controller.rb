5717e9c55dde304017ed4daa8f9916a75f03e316
Mohammad Oweis <moweis@letsevenup.com>
2016-01-12 13:35:32 -0500

    static pages complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  @base = "| Ruby on Rails Tutorial Sample App"
-  
   def hello_world
     render text: "Hello, World"
   end
