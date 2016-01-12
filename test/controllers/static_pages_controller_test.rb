23d6d7650348b93455dc531735addfab77d8712c
Mohammad Oweis <moweis@letsevenup.com>
2016-01-11 19:49:12 -0500

    embedded ruby in html

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  base = "| Ruby on Rails Tutorial Sample App"
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home #{base}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help #{base}"
   end
   
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About #{base}"
   end
 end
